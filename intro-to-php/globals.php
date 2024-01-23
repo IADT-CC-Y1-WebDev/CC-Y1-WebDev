9af4090ac3eb81070749c5a4435bba86b03b602d
John Dempsey <john.dempsey@iadt.ie>
2024-01-23 21:23:55 +0000

    Adding globals.php

---
+++ intro-to-php/globals.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<pre>";
+print('$_SERVER = ');  print_r($_SERVER);
+print('$_GET = ');     print_r($_GET);
+print('$_POST = ');    print_r($_POST);
+print('$_COOKIE = ');  print_r($_COOKIE);
+print('$_REQUEST = '); print_r($_REQUEST);
+print('$_SESSION = '); print_r($_SESSION);
+print('$_FILES = ');   print_r($_FILES);
+print('$_ENV = ');     print_r($_ENV);
+echo "</pre>";
+
+?>
+
