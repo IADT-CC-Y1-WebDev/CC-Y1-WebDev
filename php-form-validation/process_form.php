57c45ca256f08a6ad38dd8989469ffdcc6a617a9
John Dempsey <john.dempsey@iadt.ie>
2024-02-06 16:26:28 +0000

    Adding redirects with dummy validation

--- php-form-validation/process_form.php
+++ php-form-validation/process_form.php
@@ -1,5 +1,17 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+function redirect($url) {
+    header("Location: $url");
+    exit();
+}
+//-----------------------------------------------
+// you must be over 21 to view the success page
+//-----------------------------------------------
+if ($_POST['age'] >= 21) {
+    // redirect the browser to the success page
+    redirect("success.php");
+}
+else {
+    // redirect the browser back to the form
+    redirect("form.php");
+}
 ?>
