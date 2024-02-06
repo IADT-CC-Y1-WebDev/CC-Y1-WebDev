29b8ac3eedc7b95402c7e021db2358b222981988
John Dempsey <john.dempsey@iadt.ie>
2024-02-06 17:26:39 +0000

    Checking form is submitted as a POST request

--- php-form-validation/process_form.php
+++ php-form-validation/process_form.php
@@ -6,20 +6,29 @@ function redirect($url) {
     exit();
 }
 
-$validator = new ProfileFormValidator($_POST);
-$valid = $validator->validate();
-if ($valid) {
-    // redirect the browser to the success page
-    redirect("success.php");
-}
-else {
-    $errors = $validator->errors();
-    // redirect the browser back to the form
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
+try {
+    if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+        throw new Exception("Invalid request method");
     }
-    $_SESSION["form-data"] =  $_POST;
-    $_SESSION["form-errors"] = $errors;
-    redirect("form.php");
+    $validator = new ProfileFormValidator($_POST);
+    $valid = $validator->validate();
+    if ($valid) {
+        // redirect the browser to the success page
+        redirect("success.php");
+    }
+    else {
+        $errors = $validator->errors();
+        // redirect the browser back to the form
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $_SESSION["form-data"] =  $_POST;
+        $_SESSION["form-errors"] = $errors;
+        redirect("form.php");
+    }
+}
+catch (Exception $ex) {
+    echo $ex->getMessage();
+    exit();
 }
 ?>
