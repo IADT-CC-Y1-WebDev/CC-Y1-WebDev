8c97b33cb30a1b7d138dc94ad3a085a14db84916
John Dempsey <john.dempsey@iadt.ie>
2024-02-07 21:00:17 +0000

    Adding Profile.php class and testing it

---
+++ php-db/classes/Profile.php
@@ -0,0 +1,65 @@
+<?php
+
+class Profile {
+
+    public $id;
+    public $name;
+    public $age;
+    public $category;
+    public $experience;
+    public $languages;
+
+    public function __construct($props = null) {
+        if ($props != null) {
+            if (array_key_exists("id", $props)) {
+                $this->id = $props["id"];
+            }
+            $this->name = $props["name"];
+            $this->age  = $props["age"];
+            $this->category = $props["category"];
+            $this->experience  = $props["experience"];
+            $this->languages = $props["languages"];
+        }
+    }
+
+    public static function findAll() {
+        $profiles = array();
+
+        try {
+            $db = new DB();
+            $conn = $db->open();
+
+            $sql = "SELECT * FROM profiles";
+            $stmt = $conn->prepare($sql);
+            $status = $stmt->execute();
+
+            if (!$status) {
+                $error_info = $stmt->errorInfo();
+                $message = sprintf(
+                    "SQLSTATE error code: %d; error message: %s",
+                    $error_info[0],
+                    $error_info[2]
+                );
+                throw new Exception($message);  
+            }
+
+            if ($stmt->rowCount() !== 0) {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                while ($row !== FALSE) {
+                    $profile = new Profile($row);
+                    $profiles[] = $profile;
+
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                }
+            }
+        }
+        finally {
+            if ($db !== null && $db->isOpen()) {
+                $db->close();
+            }
+        }
+
+        return $profiles;
+    }
+}
+?>
--- php-db/test.php
+++ php-db/test.php
@@ -2,40 +2,13 @@
 require_once "./etc/config.php";
 
 try {
-    $db = new DB();
-    $conn = $db->open();
-
-    $sql = "SELECT * FROM profiles";
-    $stmt = $conn->prepare($sql);
-    $status = $stmt->execute();
-
-    if (!$status) {
-        $error_info = $stmt->errorInfo();
-        $message = sprintf(
-            "SQLSTATE error code: %d; error message: %s",
-            $error_info[0],
-            $error_info[2]
-        );
-        throw new Exception($message);  
-    }
-
+    $profiles = Profile::findAll();
     echo "<pre>";
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    while ($row !== FALSE) {
-        print_r($row);
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
+    print_r($profiles);
     echo "</pre>";
-
-    echo "Connected successfully to the database";
 }
 catch (PDOException $ex) {
     echo $ex->getMessage();
-}
-finally {
-    if ($db != null && $db->isOpen()) {
-        $db->close();
-    }
 }
 ?>
 
